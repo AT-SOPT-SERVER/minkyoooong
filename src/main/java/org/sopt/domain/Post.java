f1b0a2eeb6fdfb1f0d09b2b756ab38891b906953
minkyeong <haileyre000b@naver.com>
2025-04-10 01:52:03 +0900

    feature: 필수기능 1: 제목길이 검사 구현

--- src/main/java/org/sopt/domain/Post.java
+++ src/main/java/org/sopt/domain/Post.java
@@ -17,4 +17,14 @@ public class Post {
     public String getTitle() {
         return this.title;
     }
+
+    // 제목의 유효성 -> Post 객체 자체의 책임으로 domain에서 처리
+    private void validateTitle(String title) {
+        if (title == null || title.trim().isEmpty()) { // title.trim().isEmpty()로 문자열이 공백만 있는지 검사
+            throw new IllegalArgumentException("제목은 비어있을 수 없습니다.");
+        }
+        if (title.length() > 30) {
+            throw new IllegalArgumentException("제목은 30자를 넘을 수 없습니다.");
+        }
+    }
 }
